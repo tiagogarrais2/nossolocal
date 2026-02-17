279aba9cea557939debd8d0e6a6a13b91fdc8b24
Tiago Arrais <tiagoarrais1981@hotmail.com>
2026-02-17 19:07:14 -0300

    Update: Atualização de versão do service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "nosso-local-v6";
-const STATIC_CACHE = "static-v6";
-const DYNAMIC_CACHE = "dynamic-v6";
+const CACHE_NAME = "nosso-local-v7";
+const STATIC_CACHE = "static-v7";
+const DYNAMIC_CACHE = "dynamic-v7";
 
 const STATIC_FILES = [
   "/",
